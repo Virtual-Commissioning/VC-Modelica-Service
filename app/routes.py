6d372a554551bbabc3c594e808fd0264afe5600f
efjer <eusb@live.dk>
2021-11-16 10:29:48 +0100

    Add original parser

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
+from flask import request
+
 from app import app
+from app.services import modelica_simulation_service
+
 
 @app.route('/')
 @app.route('/index')
@@ -12,3 +16,9 @@ def index():
         <h2>Welcome to MSS4VC! Find the source code on <a href="https://github.com/Virtual-Commissioning/VC-Modelica-Service/">GitHub</a>!</h2>
     </body>
 </html>'''
+
+@app.route('/run_modelica_simulation', methods=['POST'])
+def run_modelica_simulation():
+    data = request.get_data()
+    results_from_modelica_simulation = modelica_simulation_service.convert_simulate_modelica(data)
+    return results_from_modelica_simulation
